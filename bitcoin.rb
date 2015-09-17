118ad40d042171ccac39be016767dee821b1fb68
jwanner1 <jacobwanner1@gmail.com>
2015-09-17 03:11:31 +0000

    some working code, issues calling arrays into integers, working code is not commented

--- bitcoin.rb
+++ bitcoin.rb
@@ -2,5 +2,18 @@ require 'json'
 require 'open-uri'
 
 data = JSON.parse(open("http://fx.priceonomics.com/v1/rates/").read)
+rates = Array.new(data.values)
 
-puts data
+# crossrates 
+
+#USD_JPY_EUR_USD = data["USD_JPY"] * data["JPY_USD"]
+
+# def crossrates(c1,c2,c3)
+#     return (c1*c2*c3)
+# end
+
+# puts crossrates(data["USD_JPY"],data["JPY_EUR"],data["EUR_USD"])
+puts rates[]
+
+
+#puts data["USD_JPY_EUR_USD"]
